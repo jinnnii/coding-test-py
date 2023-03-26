0f9fe134c492735c0b56ce69449b5daf4d1e5ed4
yejin087 <zinyizhen8@gmail.com>
2023-03-26 17:04:31 +0900

    Revert "Merge branch 'hee82' into kej"
    This reverts commit 69b1193fe85d9b372809359a43799c6081afe1f3, reversing
    changes made to caa74b28fc7a2ce89bb65b5d4fcba7b495677b83.

--- week1/숫자 비교하기.py
+++
@@ -1,3 +0,0 @@
-def solution(num1, num2):
-    answer = 1 if num1 == num2 else -1
-    return answer
--- week1/중복된 숫자 개수.py
+++
@@ -1,5 +0,0 @@
-def solution(array, n):
-    answer = 0
-    for i in array:
-        answer += 1 if i==n else 0
-    return answer
